98c975e59eb3d59525f408b18751321f72845f55
shourov hasan <shourovhasan172@gmail.com>
2022-11-09 07:59:17 +0600

    3 limite service added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -16,13 +16,46 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.secdjxe.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+
+  try{
+    const serviceCollection = client.db('creativePhotography').collection('services');
 
 
+
+    app.get('/serviceone', async (req, res) => {
+      const query = {}
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.limit(3).toArray();
+      res.send(services);
+  });
+
+
+    app.get('/services', async (req, res) => {
+      const query = {}
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+  });
+
+
+
+
+
+
+
+
+
+
+  }
+  finally{
+
+  }
+
+}
+
+run().catch(err=> console.error(err));
 
 
 app.get('/',(req, res) =>{
