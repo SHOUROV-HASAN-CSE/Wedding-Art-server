26c6487534924d7ed5288de7e580c4b1da62a564
shourov hasan <shourovhasan172@gmail.com>
2022-11-10 21:17:12 +0600

    shorting success

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ async function run(){
 
     app.get('/serviceone', async (req, res) => {
       const query = {}
-      const cursor = serviceCollection.find(query);
+      const cursor = serviceCollection.find(query).sort({"_id" : -1});
       const services = await cursor.limit(3).toArray();
       res.send(services);
   });
